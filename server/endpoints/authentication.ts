7d502db4fcb6503bf9c5c63f95d8293e38ffd569
Suki <iamsusukichan@gmail.com>
2021-08-01 21:59:45 +0200

    feat: change get request to post request

--- server/endpoints/authentication.ts
+++ server/endpoints/authentication.ts
@@ -11,8 +11,8 @@ import {
 const router = Router();
 
 // if password and email is correct returns new token
-router.get("/api/login", timeout, (req, res) => {
-  const { username, password } = req.query;
+router.post("/api/login", timeout, (req, res) => {
+  const { username, password } = req.body;
 
   const user = users.find(
     (user) => user.username === username && user.password === password
